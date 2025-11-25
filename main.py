c9ff26f900923b4958ed5f739b76e2db00acbbc9
Ivan Stepanov <stepanov_iy@cubit.su>
2025-11-25 17:48:55 +0700

    feat: add age verifyer [USER-AGE]

--- main.py
+++ main.py
@@ -12,9 +12,27 @@ def print_hi(name):
 
 
 # Press the green button in the gutter to run the script.
+def verify_age(user_name):
+    age_str = input(f"Введи свой возраст, {user_name}:\n")
+    try:
+        age = int(age_str)
+        if 0 <= age <= 100:
+            print("Ясно. :)")
+        else:
+            raise ValueError("Кажется, Вы ввели некорректный возраст")
+    except ValueError as ex:
+        raise ex
+
+
 if __name__ == '__main__':
-    user_input = input()
-    if user_input is None or not len(user_input):
-        raise Incomplete("Вы не ввели приветствие!")
-    print_hi(user_input)
+    try:
+        user_input = input()
+        if user_input is None or not len(user_input):
+            raise Incomplete()
+        print_hi(user_input)
+        verify_age(user_input)
+    except Incomplete as ex:
+        print("Вы не ввели приветствие!")
+    except Exception as ex:
+        print(f"Rsaised generic exception: {ex}")
 
