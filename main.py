f2e2993b34969bb6c33e3cccf696b06a50c98385
Ivan Stepanov <stepanov_iy@cubit.su>
2025-11-25 17:41:08 +0700

    feat: add basic user story picker [USER-STORY]

--- main.py
+++ main.py
@@ -12,9 +12,20 @@ def print_hi(name):
 
 
 # Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    user_input = input()
-    if user_input is None or not len(user_input):
-        raise Incomplete("Вы не ввели приветствие!")
-    print_hi(user_input)
+def gave_base_story(user_input):
+    if len(user_input) % 2:
+        print("Кот залез на шкаф, увидел птицу, чихнул — и упал в цветочный горшок. Все смеялись!")
+    else:
+        print("Девочка нашла монетку, купила мороженое, а оно оказалось с сюрпризом — внутри кольцо!")
 
+if __name__ == '__main__':
+    try:
+        user_input = input()
+        if user_input is None or not len(user_input):
+            raise Incomplete()
+        print_hi(user_input)
+        gave_base_story(user_input)
+    except Incomplete as ex:
+        print("Вы не ввели приветствие!")
+    except Exception as ex:
+        print(f"Rsaised generic exception: {ex}")
